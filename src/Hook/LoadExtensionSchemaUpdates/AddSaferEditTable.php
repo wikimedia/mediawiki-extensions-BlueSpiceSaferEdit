8701442ff985a6188c5d4fe280fcbbde9f8b065a
pwirth <wirth@hallowelt.biz>
2018-12-05 12:32:48 +0100

    Remove support for other db types. ERM:10576
    => Needs cherry-pick to REL1_31
    
    Change-Id: Ida88c0f43518f1e578019b9ba6f5fcd5de7393b8

--- src/Hook/LoadExtensionSchemaUpdates/AddSaferEditTable.php
+++ src/Hook/LoadExtensionSchemaUpdates/AddSaferEditTable.php
@@ -7,25 +7,20 @@ use BlueSpice\Hook\LoadExtensionSchemaUpdates;
 class AddSaferEditTable extends LoadExtensionSchemaUpdates {
 
 	protected function doProcess() {
-		$dbType = $this->updater->getDB()->getType();
-		$dir = "{$this->getExtensionPath()}/maintenance/db/$dbType";
+		$dir = "{$this->getExtensionPath()}/maintenance/db";
 
-		if ( $dbType == 'mysql' ) {
-			$this->updater->addExtensionIndex(
-				'bs_saferedit',
-				'se_page_title',
-				"$dir/bs_saferedit.patch.se_page_title.index.sql"
-			);
-			$this->updater->addExtensionIndex(
-				'bs_saferedit',
-				'se_page_namespace',
-				"$dir/bs_saferedit.patch.se_page_namespace.index.sql"
-			);
-		}
+		$this->updater->addExtensionIndex(
+			'bs_saferedit',
+			'se_page_title',
+			"$dir/bs_saferedit.patch.se_page_title.index.sql"
+		);
 
-		if ( $dbType == 'sqlite' ) {
-			$dbType = 'mysql';
-		}
+		$this->updater->addExtensionIndex(
+			'bs_saferedit',
+			'se_page_namespace',
+			"$dir/bs_saferedit.patch.se_page_namespace.index.sql"
+		);
+
 		$this->updater->addExtensionTable(
 			'bs_saferedit',
 			"$dir/bs_saferedit.sql"
