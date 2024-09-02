16693588d0de157a878b7a59ce62fa1ba5de2704
hslater <hamish.slater@hallowelt.com>
2024-09-02 20:30:13 +1200

    Use CollabPadsAfterAddContentActionHook
    Disables collabpad edit action during VE
    
    ERM37843
    
    [REL1_39, REL1_39-4.5.x, master]
    
    Depends-On: https://gitlab.hallowelt.com/HalloWelt/mediawiki-extensions-collabpads/-/merge_requests/88
    
    Change-Id: I4e1dadb7e4494a48c20bb57998c527d215e54d0d
    (cherry picked from commit 8aa7790bb33a07cbb5c3cd464f662bc0c5044beb)

---
+++ src/HookHandler/DisableCollabPadsContentAction.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace BlueSpice\SaferEdit\HookHandler;
+
+use BlueSpice\Discovery\ITemplateDataProvider;
+use Config;
+use ConfigFactory;
+use MediaWiki\Extension\CollabPads\Hook\CollabPadsAfterAddContentActionHook;
+use Title;
+use Wikimedia\Rdbms\LoadBalancer;
+
+class DisableCollabPadsContentAction implements CollabPadsAfterAddContentActionHook {
+
+	/** @var LoadBalancer */
+	private $loadBalancer;
+
+	/** @var Config */
+	private $bsgConfig;
+
+	/**
+	 * @param LoadBalancer $loadBalancer
+	 * @param ConfigFactory $configFactory
+	 */
+	public function __construct( LoadBalancer $loadBalancer, ConfigFactory $configFactory ) {
+		$this->loadBalancer = $loadBalancer;
+		$this->bsgConfig = $configFactory->makeConfig( 'bsg' );
+	}
+
+	/**
+	 * @param Title $title
+	 * @param ITemplateDataProvider $registry
+	 */
+	public function onCollabPadsAfterAddContentAction( Title $title, ITemplateDataProvider $registry ): void {
+		if ( $this->hasRecentSaferEditSession( $title ) ) {
+			// Disable collabpad action during VE
+			$registry->unregister( 'panel/edit', 'ca-collabpad' );
+		}
+	}
+
+	/**
+	 * @param Title $title
+	 * @return bool
+	 */
+	protected function hasRecentSaferEditSession( $title ): bool {
+		$interval = $this->getInterval();
+		$thresholdTS = wfTimestamp( TS_MW, time() - $interval );
+
+		$dbr = $this->loadBalancer->getConnection( DB_REPLICA );
+		$res = $dbr->newSelectQueryBuilder()
+			->table( 'bs_saferedit' )
+			->fields( 'se_id' )
+			->where( [
+				'se_page_title' => $title->getDBkey(),
+				'se_page_namespace' => $title->getNamespace(),
+				'se_timestamp > ' . $dbr->addQuotes( $thresholdTS )
+			] )
+			->caller( __METHOD__ )
+			->fetchRowCount();
+
+		return $res > 0;
+	}
+
+	/**
+	 * @return int
+	 */
+	protected function getInterval(): int {
+		$saferEditInterval = $this->bsgConfig->get( 'SaferEditInterval' );
+		$pingInterval = $this->bsgConfig->get( 'PingInterval' );
+
+		return $saferEditInterval + $pingInterval;
+	}
+}
